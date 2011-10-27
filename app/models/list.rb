48e1e0ff69ea7e56c4e23f812d5c1caec4282ccf
Andrew Nguyen <andrew13nguyen@yahoo.com>
2011-10-27 13:54:58 -0700

    route test

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,18 +1,18 @@
 class List < ActiveRecord::Base
-#   validates_presence_of :title, :episode
-#   attr_accessible :title, :episode, :ongoing
-#   
-#   def self.get_ongoing(sort = "title")
-#     sql_str = <<SQLSTR
-# SELECT `lists`.* FROM `lists` WHERE `lists`.ongoing = TRUE ORDER BY #{sort}
-# SQLSTR
-#     self.find_by_sql(sql_str)
-#   end
-#   
-#   def self.get_complete(sort = "title", order = nil)
-#     sql_str = <<SQLSTR
-# SELECT `lists`.* FROM `lists` WHERE `lists`.ongoing = FALSE ORDER BY #{sort}
-# SQLSTR
-#     self.find_by_sql(sql_str)
-#   end
+  validates_presence_of :title, :episode
+  attr_accessible :title, :episode, :ongoing
+  
+  def self.get_ongoing(sort = "title")
+    sql_str = <<SQLSTR
+SELECT `lists`.* FROM `lists` WHERE `lists`.ongoing = TRUE ORDER BY #{sort}
+SQLSTR
+    self.find_by_sql(sql_str)
+  end
+  
+  def self.get_complete(sort = "title", order = nil)
+    sql_str = <<SQLSTR
+SELECT `lists`.* FROM `lists` WHERE `lists`.ongoing = FALSE ORDER BY #{sort}
+SQLSTR
+    self.find_by_sql(sql_str)
+  end
 end
