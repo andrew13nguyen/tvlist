aaac8e61c8bcccaae0e00a29fc81f5bbfbd08372
Andrew Nguyen <andrew13nguyen@yahoo.com>
2012-01-26 19:23:05 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tv::Application.config.secret_token = 'cdce454c90e1edcdcad4a34cee8d934c9e6902dca056371580a02cd69dccfeaf40bb16669d78552c983849fb6056dce25a1e0d9c22ed7c908f295baf6d7f845b'
