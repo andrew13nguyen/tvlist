e5dabbe4f647832fe784c647d3c24e7a2c558f00
Andrew Nguyen <andrew13nguyen@yahoo.com>
2012-02-22 22:18:08 -0800

    fixed double redirect bug

--- app/controllers/tvshows_controller.rb
+++ app/controllers/tvshows_controller.rb
@@ -5,7 +5,7 @@ class TvshowsController < ApplicationController
   def require_login
     unless user_signed_in?
       # flash[:error] = "You must be logged in to access this section"
-      redirect_to redirect_to "/users/sign_in"
+      redirect_to "/users/sign_in"
     end
   end
 
