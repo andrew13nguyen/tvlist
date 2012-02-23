8239a4b6cd57762292dbc05fedce32849f6f2323
Andrew Nguyen <andrew13nguyen@yahoo.com>
2012-02-22 22:35:04 -0800

    replaced find_by_sql

--- app/models/tvshow.rb
+++ app/models/tvshow.rb
@@ -11,16 +11,18 @@ class Tvshow < ActiveRecord::Base
   end
   
   def self.get_ongoing(user_id, sort = "title")
-    sql_str = <<SQLSTR
-SELECT tvshows.* FROM tvshows WHERE tvshows.user_id = #{user_id} AND tvshows.ongoing = TRUE ORDER BY #{sort}
-SQLSTR
-    self.find_by_sql(sql_str)
+#     sql_str = <<SQLSTR
+# SELECT tvshows.* FROM tvshows WHERE tvshows.user_id = #{user_id} AND tvshows.ongoing = TRUE ORDER BY #{sort}
+# SQLSTR
+#     self.find_by_sql(sql_str)
+    Tvshow.find(:all, :conditions => ["user_id = ? and ongoing = ?", user_id, true], :order => sort)
   end
   
   def self.get_complete(user_id, sort = "title", order = nil)
-    sql_str = <<SQLSTR
-SELECT tvshows.* FROM tvshows WHERE tvshows.user_id = #{user_id} AND tvshows.ongoing = FALSE ORDER BY #{sort}
-SQLSTR
-    self.find_by_sql(sql_str)
+#     sql_str = <<SQLSTR
+# SELECT tvshows.* FROM tvshows WHERE tvshows.user_id = #{user_id} AND tvshows.ongoing = FALSE ORDER BY #{sort}
+# SQLSTR
+#     self.find_by_sql(sql_str)
+    Tvshow.find(:all, :conditions => ["user_id = ? and ongoing = ?", user_id, false], :order => sort)
   end
 end
